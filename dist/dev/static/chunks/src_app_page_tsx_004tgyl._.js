226c3ac9b77eb5b973f45aace9e05309cb3a7562
Tushar-max24 <tusharjangid98870@gmail.com>
2026-07-20 09:49:28 +0100

    chore: bust vercel cache

--- dist/dev/static/chunks/src_app_page_tsx_004tgyl._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_1rp8t-c._.js","static/chunks/node_modules_@base-ui_react_0yxrjhs._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_motion-dom_dist_es_0g6cuzy._.js","static/chunks/node_modules_framer-motion_dist_es_1_a7n4k._.js","static/chunks/node_modules_140gqxg._.js"],
-    source: "dynamic"
-});
